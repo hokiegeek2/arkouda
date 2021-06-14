5fb1fa15ee641236f2f90efe9aa98bcce6c74c20
hokiegeek2 <hokiegeek2@users.noreply.github.com>
2021-06-14 14:24:56 -0400

    Switched order of SSH_SERVERS inputs to fix race
    condition

--- start-arkouda.sh
+++ start-arkouda.sh
@@ -1,7 +1,7 @@
 sudo service ssh start
-export LOCALE_IPS=$(python3 /opt/client/arkouda/integration.py 'arkouda' 'arkouda-locale')
 
-export SSH_SERVERS="$LOCALE_IPS $MY_IP"
+export LOCALE_IPS=$(python3 /opt/client/arkouda/integration.py 'arkouda' 'arkouda-locale')
+export SSH_SERVERS="$MY_IP $LOCALE_IPS"
 
 ./arkouda_server -nl ${NUMLOCALES:-1} --memTrack=${MEMTRACK:-true} --authenticate=${AUTHENTICATE:-false} \
                  --logLevel=${LOG_LEVEL:-LogLevel.INFO}
