fa0e5c7f7c204e9599cb0b8495de51eba9f685ba
hokiegeek2 <hokiegeek2@gmail.com>
2021-06-16 22:04:40 +0000

    Updates per integration testing

--- start-arkouda-server.sh
+++ start-arkouda-server.sh
@@ -1,6 +1,15 @@
 sudo service ssh start
-export LOCALE_IPS=$(python arkouda/integration.py 'namespace="arkouda"' \
-                 'app_name="arkouda-locale"' 'pretty_print=True')
-export SSH_SERVERS=$MY_IP $LOCALE_IPS
+
+mkdir ~/.ssh/
+sudo cp ~/ssh-keys/id_rsa* ~/.ssh/
+sudo chown -R ubuntu:ubuntu ~/.ssh/*
+chmod -R 600 ~/.ssh/*
+
+cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
+
+export LOCALE_IPS=$(python3 /opt/client/arkouda/integration.py 'arkouda' 'arkouda-locale')
+export SSH_SERVERS="$MY_IP $LOCALE_IPS"
+
 ./arkouda_server -nl ${NUMLOCALES:-1} --memTrack=${MEMTRACK:-true} --authenticate=${AUTHENTICATE:-false} \
                  --logLevel=${LOG_LEVEL:-LogLevel.INFO}
+                                                           
