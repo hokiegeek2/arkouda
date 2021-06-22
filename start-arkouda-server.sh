eb290df725d7393124ce4383b40dde4bcd4b058e
hokiegeek2 <hokiegeek2@gmail.com>
2021-06-22 15:47:25 +0000

    cleanup/updates per deployment testing

--- start-arkouda-server.sh
+++ start-arkouda-server.sh
@@ -7,7 +7,7 @@ chmod -R 600 ~/.ssh/*
 
 cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
 
-export LOCALE_IPS=$(python3 /opt/client/arkouda/integration.py 'arkouda' 'arkouda-locale')
+export LOCALE_IPS="$(python3 /opt/client/arkouda/integration.py 'arkouda' 'arkouda-locale')"
 export SSH_SERVERS="$MY_IP $LOCALE_IPS"
 
 ./arkouda_server -nl ${NUMLOCALES:-1} --memTrack=${MEMTRACK:-true} --authenticate=${AUTHENTICATE:-false} \
