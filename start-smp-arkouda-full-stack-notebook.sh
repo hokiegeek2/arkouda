652b9f38401f73d99ec7eacd4a28d5cf773060f8
hokiegeek2 <hokiegeek2@gmail.com>
2022-07-11 14:43:43 +0000

    cleaned up nohup

--- start-smp-arkouda-full-stack-notebook.sh
+++ start-smp-arkouda-full-stack-notebook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup /opt/arkouda/arkouda_server -nl ${NUMLOCALES:-1} > nohup.out 2> nohup.err < /dev/null &
+nohup /opt/arkouda/arkouda_server -nl ${NUMLOCALES:-1} > /dev/null &
 
 jupyter notebook --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.token=$JUPYTER_TOKEN
                                                            
